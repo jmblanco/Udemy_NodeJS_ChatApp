7fe33d5d6c69feed1193c5fbeb6bfa76138afb78
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-16 21:40:29 +0100

    feat(scroll): Add scroll to messages
    Add scroll funcionality to messages box in order to go to last messages only if the user is on the bottom o near by.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,22 @@
 var socket = io();
+
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    // Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        //console.log('Should scroll!!!');
+        messages.scrollTop(scrollHeight);
+    }
+}
 socket.on('connect', function () {
     console.log('Connected to server');
 });
@@ -16,6 +34,7 @@ socket.on('newMessage', function (message) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage', function (locationMessage) {
@@ -28,6 +47,7 @@ socket.on('newLocationMessage', function (locationMessage) {
         createdAt: formattedTime
     });
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function (e) {
