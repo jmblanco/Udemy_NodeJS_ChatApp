163faa99a64ae2e95258ebbaf125e4bd528426c3
Jose Miguel Blanco <jmblanco1986@gmail.com>
2017-11-16 17:12:06 +0100

    feat(mustache): Add Mustache Framwork
    Include mustache framework in order to use templates in de app.

--- public/js/index.js
+++ public/js/index.js
@@ -7,21 +7,27 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-    console.log('New message:', message);
+    var template = jQuery('#message-template').html();
+
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${message.from} ${formattedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,        
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (locationMessage) {
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My Current Location</a>');
-    var formattedTime = moment(locationMessage.createdAt).format('h:mm a');    
-    li.text(`${locationMessage.from} ${formattedTime}: `);
-    a.attr('href', locationMessage.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    
+    var formattedTime = moment(locationMessage.createdAt).format('h:mm a');
+    var html = Mustache.render(template, {
+        from: locationMessage.from,
+        url: locationMessage.url,
+        createdAt: formattedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
